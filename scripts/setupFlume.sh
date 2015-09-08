d512ad1063995d0e723580aaa6ce23fbc842df38
Sean Roberts <seanorama@gmail.com>
2015-09-08 12:12:46 +0100

    flume in /tmp

--- scripts/setupFlume.sh
+++ scripts/setupFlume.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Setting up flume - Start"
-mkdir -p /root/data_pipeline_demo/input
+mkdir -p /tmp/data_pipeline_demo/input
+
 cp /etc/flume/conf/flume.conf /etc/flume/conf/flume.conf.bak
 cp /app/hadoop-data-pipeline/flume/flume.conf /etc/flume/conf/flume.conf
 echo "Setting up flume - Done"
 
 echo "Starting the Flume Agent - Start"
 cd /var/log/flume
-nohup flume-ng agent -c /etc/flume/conf -f /etc/flume/conf/flume.conf -n sandbox &
+/usr/hdp/current/flume-server/etc/rc.d/init.d/flume-agent restart
+# nohup flume-ng agent -c /etc/flume/conf -f /etc/flume/conf/flume.conf -n sandbox &
 echo ""
 echo ""
 cd
